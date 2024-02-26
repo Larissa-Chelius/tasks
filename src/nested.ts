5cd6e7b829e08fa91dbeaabad40c7ffb3c9ce899
Larissa Chelius <lchelius4@icloud.com>
2024-02-25 21:16:51 -0500

    renameQuestionById done

--- src/nested.ts
+++ src/nested.ts
@@ -168,7 +168,9 @@ export function renameQuestionById(
     targetId: number,
     newName: string
 ): Question[] {
-    return [];
+    return questions.map((question) =>
+        question.id === targetId ? { ...question, name: newName } : question
+    );
 }
 
 /***
