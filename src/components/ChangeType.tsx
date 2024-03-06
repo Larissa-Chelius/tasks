21c3b38a2dc2530006fac78655af81bda3723e63
Larissa Chelius <lchelius4@icloud.com>
2024-03-06 17:38:37 -0500

    all of task 8

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,35 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    type QuestionType = "multiple_choice_question" | "short_answer_question";
+
+    const [questionType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    const changeType = () => {
+        setQuestionType(
+            questionType === "multiple_choice_question"
+                ? "short_answer_question"
+                : "multiple_choice_question"
+        );
+    };
+
+    return (
+        <div>
+            <button onClick={changeType}>Change Type</button>
+
+            {questionType === "multiple_choice_question" && (
+                <p>Multiple Choice</p>
+            )}
+            {questionType === "short_answer_question" && <p>Short Answer</p>}
+        </div>
+    );
+
+    const [value, setValue] = useState<number>(0);
+    return (
+        <span>
+            <Button onClick={() => setValue(1 + value)}>Add One</Button>
+            to {value}.
+        </span>
+    );
 }
--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,17 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [isVisible, setIsVisible] = useState(false);
+
+    function flipVisibility(): void {
+        setIsVisible(!isVisible);
+    }
+    return (
+        <div>
+            <button onClick={flipVisibility}>Reveal Answer</button>
+
+            {}
+            {isVisible && <p>The answer is: 42</p>}
+        </div>
+    );
 }
